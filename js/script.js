177acd2a0780f933b5c0e86fad6f2ac1efe65c95
simplecalitos <simplecalitos@gmail.com>
2016-01-23 21:39:24 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,624 @@
+var scEfeArti = function(){
+				var ie = (function(){
+					var undef,rv = -1; 
+					var ua = window.navigator.userAgent;
+					var msie = ua.indexOf('MSIE ');
+					var trident = ua.indexOf('Trident/');
+
+					if (msie > 0) {
+						rv = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+					} else if (trident > 0) {
+						var rvNum = ua.indexOf('rv:');
+						rv = parseInt(ua.substring(rvNum + 3, ua.indexOf('.', rvNum)), 10);
+					}
+
+					return ((rv > -1) ? rv : undef);
+				}());
+
+				var keys = [32, 37, 38, 39, 40], wheelIter = 0;
+
+				function preventDefault(e) {
+					e = e || window.event;
+					if (e.preventDefault)
+					e.preventDefault();
+					e.returnValue = false;  
+				}
+
+				function keydown(e) {
+					for (var i = keys.length; i--;) {
+						if (e.keyCode === keys[i]) {
+							preventDefault(e);
+							return;
+						}
+					}
+				}
+
+				function touchmove(e) {
+					preventDefault(e);
+				}
+
+				function wheel(e) {
+					// for IE 
+					//if( ie ) {
+						//preventDefault(e);
+					//}
+				}
+
+				function disable_scroll() {
+					window.onmousewheel = document.onmousewheel = wheel;
+					document.onkeydown = keydown;
+					document.body.ontouchmove = touchmove;
+				}
+
+				function enable_scroll() {
+					window.onmousewheel = document.onmousewheel = document.onkeydown = document.body.ontouchmove = null;  
+				}
+
+				var docElem = window.document.documentElement,
+					scrollVal,
+					isRevealed, 
+					noscroll, 
+					isAnimating,
+					container = document.getElementById( 'scEfeArtiContainer' ),
+					trigger = container.querySelector( 'button.trigger' );
+
+				function scrollY() {
+					return window.pageYOffset || docElem.scrollTop;
+				}
+				
+				function scrollPage() {
+					scrollVal = scrollY();
+					
+					if( noscroll && !ie ) {
+						if( scrollVal < 0 ) return false;
+						window.scrollTo( 0, 0 );
+					}
+
+					if( classie.has( container, 'notrans' ) ) {
+						classie.remove( container, 'notrans' );
+						return false;
+					}
+
+					if( isAnimating ) {
+						return false;
+					}
+					
+					if( scrollVal <= 0 && isRevealed ) {
+						toggle(0);
+					}
+					else if( scrollVal > 0 && !isRevealed ){
+						toggle(1);
+					}
+				}
+
+				function toggle( reveal ) {
+					isAnimating = true;
+					
+					if( reveal ) {
+						classie.add( container, 'modify' );
+					}
+					else {
+						noscroll = true;
+						disable_scroll();
+						classie.remove( container, 'modify' );
+					}
+
+					setTimeout( function() {
+						isRevealed = !isRevealed;
+						isAnimating = false;
+						if( reveal ) {
+							noscroll = false;
+							enable_scroll();
+						}
+					}, 600 );
+				}
+
+				var pageScroll = scrollY();
+				noscroll = pageScroll === 0;
+				
+				disable_scroll();
+				
+				if( pageScroll ) {
+					isRevealed = true;
+					classie.add( container, 'notrans' );
+					classie.add( container, 'modify' );
+				}
+				
+				window.addEventListener( 'scroll', scrollPage );
+				trigger.addEventListener( 'click', function() { toggle( 'reveal' ); } );
+};
+var scEfeBack1 = function(pLargeHeader, pCanvas){
+   var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        target = {x: width/2, y: height/2};
+
+        largeHeader = document.getElementById(pLargeHeader);
+        largeHeader.style.height = height+'px';
+
+        canvas = document.getElementById(pCanvas);
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+        points = [];
+        for(var x = 0; x < width; x = x + width/20) {
+            for(var y = 0; y < height; y = y + height/20) {
+                var px = x + Math.random()*width/20;
+                var py = y + Math.random()*height/20;
+                var p = {x: px, originX: px, y: py, originY: py };
+                points.push(p);
+            }
+        }
+
+        for(var i = 0; i < points.length; i++) {
+            var closest = [];
+            var p1 = points[i];
+            for(var j = 0; j < points.length; j++) {
+                var p2 = points[j]
+                if(!(p1 == p2)) {
+                    var placed = false;
+                    for(var k = 0; k < 5; k++) {
+                        if(!placed) {
+                            if(closest[k] == undefined) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+
+                    for(var k = 0; k < 5; k++) {
+                        if(!placed) {
+                            if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+                }
+            }
+            p1.closest = closest;
+        }
+
+        for(var i in points) {
+            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
+            points[i].circle = c;
+        }
+    }
+
+    function addListeners() {
+		/*
+        if(!('ontouchstart' in window)) {
+            window.addEventListener('mousemove', mouseMove);
+        }
+		
+        window.addEventListener('scroll', scrollCheck);
+      window.addEventListener('resize', resize);
+	*/
+    }
+
+    function mouseMove(e) {
+        var posx = posy = 0;
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        }
+        else if (e.clientX || e.clientY)    {
+            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        target.x = posx;
+        target.y = posy;
+    }
+
+    function scrollCheck() {
+
+	   if(document.body.scrollTop > height) animateHeader = false;
+        else animateHeader = true;
+    }
+
+    function resize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        largeHeader.style.height = height+'px';
+        canvas.width = width;
+        canvas.height = height;
+    }
+
+    function initAnimation() {
+        animate();
+        for(var i in points) {
+            shiftPoint(points[i]);
+        }
+    }
+
+    function animate() {
+        if(animateHeader) {
+            ctx.clearRect(0,0,width,height);
+            for(var i in points) {
+                if(Math.abs(getDistance(target, points[i])) < 4000) {
+                    points[i].active = 0.3;
+                    points[i].circle.active = 0.6;
+                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
+                    points[i].active = 0.1;
+                    points[i].circle.active = 0.3;
+                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
+                    points[i].active = 0.02;
+                    points[i].circle.active = 0.1;
+                } else {
+                    points[i].active = 0;
+                    points[i].circle.active = 0;
+                }
+
+                drawLines(points[i]);
+                points[i].circle.draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    function shiftPoint(p) {
+        TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
+            y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
+            onComplete: function() {
+                shiftPoint(p);
+            }});
+    }
+
+    function drawLines(p) {
+        if(!p.active) return;
+        for(var i in p.closest) {
+            ctx.beginPath();
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(p.closest[i].x, p.closest[i].y);
+            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+            ctx.stroke();
+        }
+    }
+
+    function Circle(pos,rad,color) {
+        var _this = this;
+
+        (function() {
+            _this.pos = pos || null;
+            _this.radius = rad || null;
+            _this.color = color || null;
+        })();
+
+        this.draw = function() {
+            if(!_this.active) return;
+            ctx.beginPath();
+            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fill();
+        };
+    }
+
+    function getDistance(p1, p2) {
+        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+    }
+	
+	initHeader();
+    initAnimation();
+    addListeners();
+};
+
+var scEfeBack2 = function(pLargeHeader, pCanvas){
+	var width, height, largeHeader, canvas, ctx, circles, target, animateHeader = true;
+   
+    initHeader();
+    addListeners();
+
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        target = {x: 0, y: height};
+
+        largeHeader = document.getElementById(pLargeHeader);
+        largeHeader.style.height = height+'px';
+
+        canvas = document.getElementById(pCanvas);
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+        circles = [];
+        for(var x = 0; x < width*0.5; x++) {
+            var c = new Circle();
+            circles.push(c);
+        }
+        animate();
+    }
+
+    function addListeners() {
+	/*
+       window.addEventListener('scroll', scrollCheck);
+       window.addEventListener('resize', resize);
+	   */
+    }
+
+    function scrollCheck() {
+        if(document.body.scrollTop > height) animateHeader = false;
+        else animateHeader = true;
+    }
+
+    function resize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        largeHeader.style.height = height+'px';
+        canvas.width = width;
+        canvas.height = height;
+    }
+
+    function animate() {
+        if(animateHeader) {
+            ctx.clearRect(0,0,width,height);
+            for(var i in circles) {
+                circles[i].draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    function Circle() {
+        var _this = this;
+
+        (function() {
+            _this.pos = {};
+            init();
+            console.log(_this);
+        })();
+
+        function init() {
+            _this.pos.x = Math.random()*width;
+            _this.pos.y = height+Math.random()*100;
+            _this.alpha = 0.1+Math.random()*0.3;
+            _this.scale = 0.1+Math.random()*0.3;
+            _this.velocity = Math.random();
+        }
+
+        this.draw = function() {
+            if(_this.alpha <= 0) {
+                init();
+            }
+            _this.pos.y -= _this.velocity;
+            _this.alpha -= 0.0005;
+            ctx.beginPath();
+            ctx.arc(_this.pos.x, _this.pos.y, _this.scale*10, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(255,255,255,'+ _this.alpha+')';
+            ctx.fill();
+        };
+    }
+};	
+
+var scEfeBack3 = function(pLargeHeader, pCanvas){
+	var width, height, largeHeader, canvas, ctx, triangles, target, animateHeader = true;
+    var colors = ['72,35,68', '43,81,102', '66,152,103', '250,178,67', '224,33,48'];
+
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        target = {x: 0, y: height};
+
+        largeHeader = document.getElementById(pLargeHeader);
+        largeHeader.style.height = height+'px';
+
+        canvas = document.getElementById(pCanvas);
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+        triangles = [];
+        for(var x = 0; x < 480; x++) {
+            addTriangle(x*10);
+        }
+    }
+
+    function addTriangle(delay) {
+        setTimeout(function() {
+            var t = new Triangle();
+            triangles.push(t);
+            tweenTriangle(t);
+        }, delay);
+    }
+
+    function initAnimation() {
+        animate();
+    }
+
+    function tweenTriangle(tri) {
+        var t = Math.random()*(2*Math.PI);
+        var x = (200+Math.random()*100)*Math.cos(t) + width*0.5;
+        var y = (200+Math.random()*100)*Math.sin(t) + height*0.5-20;
+        var time = 4+3*Math.random();
+
+        TweenLite.to(tri.pos, time, {x: x,
+            y: y, ease:Circ.easeOut,
+            onComplete: function() {
+                tri.init();
+                tweenTriangle(tri);
+        }});
+    }
+
+    function addListeners() {
+		/*
+        window.addEventListener('scroll', scrollCheck);
+        window.addEventListener('resize', resize);
+		*/
+    }
+
+    function scrollCheck() {
+        if(document.body.scrollTop > height) animateHeader = false;
+        else animateHeader = true;
+    }
+
+    function resize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        largeHeader.style.height = height+'px';
+        canvas.width = width;
+        canvas.height = height;
+    }
+
+    function animate() {
+        if(animateHeader) {
+            ctx.clearRect(0,0,width,height);
+            for(var i in triangles) {
+                triangles[i].draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    function Triangle() {
+        var _this = this;
+
+        (function() {
+            _this.coords = [{},{},{}];
+            _this.pos = {};
+            init();
+        })();
+
+        function init() {
+            _this.pos.x = width*0.5;
+            _this.pos.y = height*0.5-20;
+            _this.coords[0].x = -10+Math.random()*40;
+            _this.coords[0].y = -10+Math.random()*40;
+            _this.coords[1].x = -10+Math.random()*40;
+            _this.coords[1].y = -10+Math.random()*40;
+            _this.coords[2].x = -10+Math.random()*40;
+            _this.coords[2].y = -10+Math.random()*40;
+            _this.scale = 0.1+Math.random()*0.3;
+            _this.color = colors[Math.floor(Math.random()*colors.length)];
+            setTimeout(function() { _this.alpha = 0.8; }, 10);
+        }
+
+        this.draw = function() {
+            if(_this.alpha >= 0.005) _this.alpha -= 0.005;
+            else _this.alpha = 0;
+            ctx.beginPath();
+            ctx.moveTo(_this.coords[0].x+_this.pos.x, _this.coords[0].y+_this.pos.y);
+            ctx.lineTo(_this.coords[1].x+_this.pos.x, _this.coords[1].y+_this.pos.y);
+            ctx.lineTo(_this.coords[2].x+_this.pos.x, _this.coords[2].y+_this.pos.y);
+            ctx.closePath();
+            ctx.fillStyle = 'rgba('+_this.color+','+ _this.alpha+')';
+            ctx.fill();
+        };
+
+        this.init = init;
+    }
+	
+	initHeader();
+    addListeners();
+    initAnimation();
+};
+
+var scEfeBack4 = function(pLargeHeader, pCanvas){
+	 var width, height, largeHeader, canvas, ctx, lines, target, size, animateHeader = true;
+
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        size = width > height ? height : width;
+        target = {x: 0, y: height};
+
+        largeHeader = document.getElementById(pLargeHeader);
+        largeHeader.style.height = height+'px';
+
+        canvas = document.getElementById(pCanvas);
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+        lines = [];
+        for(var i = 0; i < 90; i++) {
+            var l = new Line(Math.random()*360);
+            lines.push(l);
+        }
+    }
+
+    function initAnimation() {
+        animate();
+    }
+
+    function addListeners() {
+	   /*
+       window.addEventListener('scroll', scrollCheck);
+       window.addEventListener('resize', resize);
+	   */
+    }
+
+    function scrollCheck() {
+        if(document.body.scrollTop > height) animateHeader = false;
+        else animateHeader = true;
+    }
+
+    function resize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        size = width > height ? height : width;
+        largeHeader.style.height = height+'px';
+        canvas.width = width;
+        canvas.height = height;
+    }
+
+    function animate() {
+        if(animateHeader) {
+            ctx.clearRect(0,0,width,height);
+            for(var i in lines) {
+                lines[i].draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    function Line(angle) {
+        var _this = this;
+		
+        (function() {
+            _this.angle = angle;
+
+        })();
+
+        this.draw = function() {
+
+            var r1 = Math.random()*(size < 400 ? 400 : size)*0.4;
+            var r2 = Math.random()*(size < 400 ? 400 : size)*0.4;
+            var x1 = r1*Math.cos(_this.angle*(Math.PI/180)) + width*0.5;
+            var y1 = r1*Math.sin(_this.angle*(Math.PI/180)) + height*0.48;
+            var x2 = r2*Math.cos(_this.angle*(Math.PI/180)) + width*0.5;
+            var y2 = r2*Math.sin(_this.angle*(Math.PI/180)) + height*0.48;
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.strokeStyle = 'rgba(255,193,127,'+(0.5+Math.random()*0.5)+')';
+
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(x1, y1, 2, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(255,165,70,'+(0.5+Math.random()*0.5)+')';
+            ctx.fill();
+
+            _this.angle += Math.random();
+        };
+    }
+
+	initHeader();
+    addListeners();
+    initAnimation();
+};
+
+(function() {
+scEfeArti();
+	var t = document.getElementsByClassName("large-header").length;
+	if(t>0){
+		for(var i=1; i<=t; i++){
+			var l = "large-header"+i;
+			var d = "demo-canvas"+i;
+			eval("scEfeBack"+i+"('"+l+"', '"+d+"') ");
+		}
+	}
+})();
